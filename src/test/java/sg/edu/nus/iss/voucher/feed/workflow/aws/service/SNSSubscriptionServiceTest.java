6f7f0ad68fa9181a0a85018be066ef177a29b18b
Thet-Nandar-Aung <e1327955@u.nus.edu>
2024-10-13 13:56:11 +0800

    Code Refactoring and modify the test case.

--- src/test/java/sg/edu/nus/iss/voucher/feed/workflow/aws/service/SNSSubscriptionServiceTest.java
+++ src/test/java/sg/edu/nus/iss/voucher/feed/workflow/aws/service/SNSSubscriptionServiceTest.java
@@ -1,84 +1,87 @@
 package sg.edu.nus.iss.voucher.feed.workflow.aws.service;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
-
-import org.json.simple.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.web.client.RestTemplate;
 
 import sg.edu.nus.iss.voucher.feed.workflow.dao.FeedDAO;
 import sg.edu.nus.iss.voucher.feed.workflow.dto.LiveFeedDTO;
 import sg.edu.nus.iss.voucher.feed.workflow.entity.Feed;
 import sg.edu.nus.iss.voucher.feed.workflow.entity.MessagePayload;
 import sg.edu.nus.iss.voucher.feed.workflow.entity.TargetUser;
-import sg.edu.nus.iss.voucher.feed.workflow.strategy.impl.*;
-import sg.edu.nus.iss.voucher.feed.workflow.utility.DTOMapper;
+import sg.edu.nus.iss.voucher.feed.workflow.strategy.impl.EmailStrategy;
+import sg.edu.nus.iss.voucher.feed.workflow.strategy.impl.NotificationStrategy;
 import sg.edu.nus.iss.voucher.feed.workflow.utility.JSONReader;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest 
 @ActiveProfiles("test")
 public class SNSSubscriptionServiceTest {
 
-	@InjectMocks
-	private SNSSubscriptionService snsSubscriptionService;
+    @Mock
+    private JSONReader jsonReader;
 
-	@Mock
-	private JSONReader jsonReader;
+    @Mock
+    private FeedDAO feedDAO;
 
-	@Mock
-	private FeedDAO feedDAO;
+    @Mock
+    private EmailStrategy emailStrategy;
 
-	@Mock
-	private EmailStrategy emailStrategy;
+    @Mock
+    private NotificationStrategy notificationStrategy;
 
-	@Mock
-	private NotificationStrategy notificationStrategy;
+    @InjectMocks
+    private SNSSubscriptionService snsSubscriptionService;
 
-	@Mock
-	private RestTemplate restTemplate;
-	
-	@Mock
-    private DTOMapper dtoMapper;
-	
-	static String userId ="user123";
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
-	@BeforeEach
-	public void setUp() {
-		MockitoAnnotations.openMocks(this);
-	}
+    @Test
+    public void testProcessNotification() throws Exception {
+     
+        String snsMessage = "{\n"
+        		+ "    \"Type\": \"Notification\",\n"
+        		+ "    \"MessageId\": \"example-message-id\",\n"
+        		+ "    \"TopicArn\": \"arn:aws:sns:region:account-id:topic-name\",\n"
+        		+ "    \"Message\": \"{\\\"category\\\": \\\"Food\\\", \\\"campaign\\\": {\\\"campaignId\\\": \\\"123\\\", \\\"description\\\": \\\"Happy Hour\\\"}, \\\"store\\\": {\\\"storeId\\\": \\\"456\\\", \\\"name\\\": \\\"MUJI\\\"}}\",\n"
+        		+ "    \"Timestamp\": \"2024-09-08T12:34:56.789Z\",\n"
+        		+ "    \"SignatureVersion\": \"1\",\n"
+        		+ "    \"Signature\": \"example-signature\",\n"
+        		+ "    \"SigningCertURL\": \"https://sns-region.amazonaws.com/SimpleNotificationService-1234567890.pem\",\n"
+        		+ "    \"UnsubscribeURL\": \"https://sns-region.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:region:account-id:topic-name:subscription-id\",\n"
+        		+ "    \"MessageAttributes\": {\n"
+        		+ "        \"X-User-Id\": {\n"
+        		+ "            \"Type\": \"String\",\n"
+        		+ "            \"Value\": \"41e0c9f2-81a4-4b44-b084-90fc0c4cd35b\"\n"
+        		+ "        }\n"
+        		+ "    }\n"
+        		+ "}";
+        MessagePayload feedMsg = new MessagePayload();
+        feedMsg.setCategory("news");
+        feedMsg.setCampaignId("123");
+        feedMsg.setCampaignDescription("Test Campaign");
+        feedMsg.setStoreId("456");
+        feedMsg.setStoreName("Test Store");
 
-	@Test
-	public void testProcessNotification() throws Exception {
-		String message = "{\"Message\": \"Test message\"}";
-		MessagePayload feedMsg = new MessagePayload();
-		feedMsg.setCategory("Food");
-		feedMsg.setCampaignId("1");
-		feedMsg.setCampaignDescription("Campaign Description");
-		feedMsg.setStoreId("StoreId");
-		feedMsg.setStoreName("StoreName");
-
-		ArrayList<TargetUser> users = new ArrayList<>();
-		TargetUser user = new TargetUser();
-		user.setUserId("11");
+        ArrayList<TargetUser> users = new ArrayList<>();
+        TargetUser user = new TargetUser();
+        user.setUserId("11");
 		user.setEmail("eleven.11@gmail.com");
 		user.setUsername("User 11");
 		users.add(user);
-
-		Feed feed = new Feed();
+        
+        Feed feed = new Feed();
 		feed.setCategory(feedMsg.getCategory());
 		feed.setUserId(user.getUserId());
 		feed.setUserName(user.getUsername());
@@ -88,17 +91,22 @@ public class SNSSubscriptionServiceTest {
 		feed.setStoreId(feedMsg.getStoreId());
 		feed.setStoreName(feedMsg.getStoreName());
 
-		when(jsonReader.readFeedMessage(anyString())).thenReturn(feedMsg);
-		when(jsonReader.getUsersByPreferences(feedMsg.getCategory())).thenReturn(users);
-		when(feedDAO.checkFeedExistsByUserAndCampaign(anyString(), anyString())).thenReturn(true);
-		when(feedDAO.saveFeed(any())).thenReturn(feed);
-		when(notificationStrategy.sendNotification(any())).thenReturn(true);
-		when(emailStrategy.sendNotification(any())).thenReturn(true);
+      
 
-		String result = snsSubscriptionService.processNotification(message);
+        when(jsonReader.readFeedMessage(anyString())).thenReturn(feedMsg);
+        when(jsonReader.getUsersByPreferences(anyString())).thenReturn(users);
+        when(feedDAO.checkFeedExistsByUserAndCampaign(anyString(), anyString())).thenReturn(true);
+        when(feedDAO.saveFeed(any(Feed.class))).thenReturn(feed); 
+        when(notificationStrategy.sendNotification(any(LiveFeedDTO.class))).thenReturn(true);
+        when(emailStrategy.sendNotification(any(LiveFeedDTO.class))).thenReturn(true);
 
-		assertEquals("Processed user:11:true", result);
-	}
+     
+        String result = snsSubscriptionService.processNotification(snsMessage);
 
-	
+      
+        assertTrue(result.contains("Processed user:"));
+        
+    }
+
 }
+
